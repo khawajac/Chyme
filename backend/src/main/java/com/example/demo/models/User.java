3e04f385a9d41449b0e7699c6d621b7d9d2a61ef
Charisma <actuallycharisma@outlook.com>
2024-08-13 22:56:47 +0100

    removed unsupported erd file type

--- backend/src/main/java/com/example/demo/models/User.java
+++ backend/src/main/java/com/example/demo/models/User.java
@@ -1,4 +1,23 @@
 package com.example.demo.models;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name ="username", unique = true, nullable = false)
+    private String username;
+
+    @Column(name ="email", unique = true, nullable = false)
+    private String email;
+
+    @Column(name="password", nullable = false)
+    private String password;
+
+
 }

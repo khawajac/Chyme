92412377ddc20ee64424d7ea253fb9a77b33b86e
Charisma <actuallycharisma@outlook.com>
2024-08-14 12:45:57 +0100

    made message model

--- backend/src/main/java/com/example/demo/models/Message.java
+++ backend/src/main/java/com/example/demo/models/Message.java
@@ -1,4 +1,78 @@
 package com.example.demo.models;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "messages")
 public class Message {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name ="content", nullable = false)
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "sender_id", nullable = false)
+    private User sender;
+
+    @ManyToOne
+    @JoinColumn(name = "room_id", nullable = false)
+    private Room room;
+
+    @Column(name="time_sent", nullable = false)
+    private LocalDateTime timeStamp;
+
+    public Message(String content, User sender, Room room, LocalDateTime timeStamp) {
+        this.content = content;
+        this.sender = sender;
+        this.room = room;
+        this.timeStamp = timeStamp;
+    }
+
+    public Message() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public User getSender() {
+        return sender;
+    }
+
+    public void setSender(User sender) {
+        this.sender = sender;
+    }
+
+    public Room getRoom() {
+        return room;
+    }
+
+    public void setRoom(Room room) {
+        this.room = room;
+    }
+
+    public LocalDateTime getTimeStamp() {
+        return timeStamp;
+    }
+
+    public void setTimeStamp(LocalDateTime timeStamp) {
+        this.timeStamp = timeStamp;
+    }
 }

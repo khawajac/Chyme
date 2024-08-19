7ecea024fc823568da1e269e78c3d8903252094c
Charisma <actuallycharisma@outlook.com>
2024-08-19 21:02:58 +0100

    added a role to the user model and created an enum with USER and ADMIN

--- backend/src/main/java/com/example/demo/models/User.java
+++ backend/src/main/java/com/example/demo/models/User.java
@@ -2,7 +2,11 @@ package com.example.demo.models;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Data
 @Builder
@@ -16,16 +20,39 @@ public class User implements UserDetails {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
-    @Setter
     @Column(name ="username", unique = true, nullable = false)
     private String username;
 
-    @Setter
     @Column(name ="email", unique = true, nullable = false)
     private String email;
 
-    @Setter
     @Column(name="password", nullable = false)
     private String password;
 
+    private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
 }

a22a3670e82792d0c8cc26d045c4a82e0f962779
Charisma <actuallycharisma@outlook.com>
2024-08-19 22:16:40 +0100

    created getSignInKey method

--- backend/src/main/java/com/example/demo/config/JwtService.java
+++ backend/src/main/java/com/example/demo/config/JwtService.java
@@ -2,7 +2,11 @@ package com.example.demo.config;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
 
 @Service
 public class JwtService {
@@ -20,5 +24,10 @@ public class JwtService {
                 .build()
                 .parseClaimsJws(token)
                 .getBody();
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }

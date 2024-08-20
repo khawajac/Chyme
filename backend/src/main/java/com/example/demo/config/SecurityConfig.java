d49b6ec059c39dca4837a42ac2bf4affd325785e
Charisma <actuallycharisma@outlook.com>
2024-08-20 17:55:22 +0100

    deleted then added a line again iin sec config

--- backend/src/main/java/com/example/demo/config/SecurityConfig.java
+++ backend/src/main/java/com/example/demo/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.example.demo.config;
 
-import com.example.demo.config.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;

6d60421e445d4a48673fcee07c5764e7699612a1
Charisma <actuallycharisma@outlook.com>
2024-08-20 15:05:35 +0100

    outdated version of userAuth

--- backend/src/main/java/com/example/demo/auth/AuthenticationController.java
+++ backend/src/main/java/com/example/demo/auth/AuthenticationController.java
@@ -8,20 +8,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/auth")
+@RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
 public class AuthenticationController {
 
-    private final AuthenticationService authenticationService;
+    private final AuthenticationService service;
 
     @PostMapping("/register")
-    public ResponseEntity<AuthenticationResponse> registerUser(@RequestBody RegisterRequest request){
-        return ResponseEntity.ok(authenticationService.register(request));
+    public ResponseEntity<AuthenticationResponse> register(
+            @RequestBody RegisterRequest request
+    ) {
+        return ResponseEntity.ok(service.register(request));
     }
-
     @PostMapping("/authenticate")
-    public ResponseEntity<AuthenticationResponse> registerUser(@RequestBody AuthenticateRequest request){
-        return ResponseEntity.ok(authenticationService.authenticate(request));
-
+    public ResponseEntity<AuthenticationResponse> authenticate(
+            @RequestBody AuthenticateRequest request
+    ) {
+        return ResponseEntity.ok(service.authenticate(request));
     }
+
 }
--- backend/src/main/java/com/example/demo/auth/AuthenticationResponse.java
+++ backend/src/main/java/com/example/demo/auth/AuthenticationResponse.java
@@ -1,6 +1,7 @@
 package com.example.demo.auth;
 
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,6 +13,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class AuthenticationResponse {
 
+    @JsonProperty("token")
     private String token;
 
 }
